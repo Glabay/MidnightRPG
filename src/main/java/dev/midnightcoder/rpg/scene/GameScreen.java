b8a5b48b1c880743ab7e8ec6a0878487854c745b
Glabay <java_guru@live.com>
2026-05-01 02:51:51 -0300

    chore: moved the game scenes from the engine to the game
    Signed-off-by: Glabay <Java_Guru@live.com>

---
+++ src/main/java/dev/midnightcoder/rpg/scene/GameScreen.java
@@ -0,0 +1,12 @@
+package dev.midnightcoder.rpg.scene;
+
+import dev.midnightcoder.engine.scene.Scene;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project MidnightEngine
+ * @social Discord: Glabay
+ * @since 2026-04-30
+ */
+public class GameScreen extends Scene {
+}
