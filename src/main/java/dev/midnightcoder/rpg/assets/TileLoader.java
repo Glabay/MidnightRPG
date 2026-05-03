6d73735c88088fa390f4aadb1d33f860af4444c2
Glabay <Java_Guru@live.com>
2026-05-03 16:29:03 -0300

    chore: refactor how tiles are loaded
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/dev/midnightcoder/rpg/assets/TileLoader.java
+++ src/main/java/dev/midnightcoder/rpg/assets/TileLoader.java
@@ -7,6 +7,8 @@ import dev.midnightcoder.engine.world.tile.TileColor;
 import dev.midnightcoder.engine.world.tile.TileColorRegistry;
 import dev.midnightcoder.engine.world.tile.TileType;
 
+import java.util.List;
+
 /**
  * @author Glabay | Glabay-Studios
  * @project MidnightRPG
@@ -14,29 +16,6 @@ import dev.midnightcoder.engine.world.tile.TileType;
  * @since 2026-05-02
  */
 public class TileLoader {
-    // Mock textures (replace later with real sprites)
-    Texture grassTex = TextureFactory.createFromImageFile("/world/tiles/ground_grass.png");
-    Texture wallStoneTex  = TextureFactory.createFromImageFile("/world/tiles/wall_stone.png");
-    Texture wallStoneCapTex  = TextureFactory.createFromImageFile("/world/tiles/wall_stone_cap.png");
-    Texture wallWoodenTex  = TextureFactory.createFromImageFile("/world/tiles/wall_wood.png");
-    Texture wallWoodenCapTex  = TextureFactory.createFromImageFile("/world/tiles/wall_wood_cap.png");
-    Texture waterTex = TextureFactory.createFromImageFile("/world/tiles/water_full.png");
-    Texture farmPlotTex = TextureFactory.createFromImageFile("/world/tiles/farm_plot.png");
-
-    Texture dirtPathTex = TextureFactory.createFromImageFile("/world/tiles/ground_dirt.png");
-    Texture stonePathTex = TextureFactory.createFromImageFile("/world/tiles/ground_stone.png");
-    Texture woodFloorTex = TextureFactory.createFromImageFile("/world/tiles/floor_wood.png");
-
-    // Define tile types
-    TileType GRASS = new TileType("ground_grass", grassTex, CollisionFlag.NONE.getMask());
-    TileType WALL_STONE  = new TileType("wall_stone", wallStoneTex, CollisionFlag.FULL.getMask());
-    TileType WALL_STONE_CAP  = new TileType("wall_stone_cap", wallStoneCapTex, CollisionFlag.FULL.getMask());
-    TileType WALL_WOODEN  = new TileType("wall_wood", wallWoodenTex, CollisionFlag.FULL.getMask());
-    TileType WALL_WOODEN_CAP  = new TileType("wall_wood_cap", wallWoodenCapTex, CollisionFlag.FULL.getMask());
-    TileType STONE_PATH = new TileType("ground_stone", stonePathTex, CollisionFlag.NONE.getMask());
-    TileType WOOD_FLOOR = new TileType("floor_wood", woodFloorTex, CollisionFlag.NONE.getMask());
-    TileType WATER = new TileType("water_full", waterTex, CollisionFlag.FULL.getMask());
-    TileType FARM_PLOT = new TileType("farm_plot", farmPlotTex, CollisionFlag.NONE.getMask());
 
     private static TileLoader instance;
 
@@ -47,16 +26,25 @@ public class TileLoader {
         return instance;
     }
 
+    private TileType getTileType(String textureName, CollisionFlag collisionFlag) {
+        return new TileType(textureName, getTileTexture(textureName), collisionFlag.getMask());
+    }
+
+    private Texture getTileTexture(String textureName) {
+        return TextureFactory.createFromImageFile("/world/tiles/" + textureName + ".png");
+    }
+
     public void loadTiles() {
+        // Load in textures
         var registry = TileColorRegistry.getInstance();
-            registry.register(TileColor.GRASS_PLAIN, GRASS);
-            registry.register(TileColor.STONE_WALL, WALL_STONE);
-            registry.register(TileColor.STONE_WALL_TOP, WALL_STONE_CAP);
-            registry.register(TileColor.WOOD_WALL, WALL_WOODEN);
-            registry.register(TileColor.WOOD_WALL_TOP, WALL_WOODEN_CAP);
-            registry.register(TileColor.STONE_PATH, STONE_PATH);
-            registry.register(TileColor.WOOD_FLOOR, WOOD_FLOOR);
-            registry.register(TileColor.WATER_TILE, WATER);
-            registry.register(TileColor.GARDEN_PATCH, FARM_PLOT);
+            registry.register(TileColor.GRASS_PLAIN, getTileType("ground_grass", CollisionFlag.NONE));
+            registry.register(TileColor.STONE_WALL, getTileType("wall_stone", CollisionFlag.FULL));
+            registry.register(TileColor.STONE_WALL_TOP, getTileType("wall_stone_cap", CollisionFlag.FULL));
+            registry.register(TileColor.WOOD_WALL, getTileType("wall_wood", CollisionFlag.FULL));
+            registry.register(TileColor.WOOD_WALL_TOP, getTileType("wall_wood_cap", CollisionFlag.FULL));
+            registry.register(TileColor.STONE_PATH, getTileType("ground_stone", CollisionFlag.NONE));
+            registry.register(TileColor.WOOD_FLOOR, getTileType("floor_wood", CollisionFlag.NONE));
+            registry.register(TileColor.WATER_TILE, getTileType("water_full", CollisionFlag.FULL));
+            registry.register(TileColor.GARDEN_PATCH, getTileType("farm_plot", CollisionFlag.NONE));
     }
 }
