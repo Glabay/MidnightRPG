15898417b633c672e05c5e2476e4aae5e560994b
Glabay <Java_Guru@live.com>
2026-05-03 22:31:31 -0300

    feat: added world coordinates to the entity class
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/dev/midnightcoder/rpg/entity/Entity.java
+++ src/main/java/dev/midnightcoder/rpg/entity/Entity.java
@@ -9,6 +9,17 @@ import dev.midnightcoder.engine.renderer.Renderer;
  * @since 2026-05-01
  */
 public abstract class Entity {
+    protected int worldX;
+    protected int worldY;
+
     public void update(double delta) {}
     public void render(Renderer renderer) {}
+
+    public int getX() {
+        return worldX;
+    }
+
+    public int getY() {
+        return worldY;
+    }
 }
