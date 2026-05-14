0e4bdf1c44d965239edd29a6958d28dc9fe98d35
Glabay <Java_Guru@live.com>
2026-05-14 03:50:32 -0300

    chore: Item ID file for simplicity
    Signed-off-by: Glabay <Java_Guru@live.com>

---
+++ src/main/java/dev/midnightcoder/rpg/util/ItemId.java
@@ -0,0 +1,23 @@
+package dev.midnightcoder.rpg.util;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project MidnightRPG
+ * @social Discord: Glabay
+ * @since 2026-05-14
+ */
+public class ItemId {
+    public static final int HEALTH_POTION = 0;
+    public static final int EMPTY_VIAL = 1;
+    public static final int WOODEN_STAFF = 2;
+    public static final int WOODEN_AXE = 3;
+    public static final int STONE_PICKAXE = 4;
+    public static final int IRON_PICKAXE = 5;
+    public static final int STEEL_PICKAXE = 6;
+    public static final int STONE_AXE = 7;
+    public static final int STEEL_AXE = 8;
+    public static final int IRON_AXE = 9;
+    public static final int IRON_SWORD = 10;
+    public static final int NORMAL_LOGS = 11;
+    public static final int IRON_ORE = 12;
+}
