919bdd483755594c36b4cb2fdb1c8f4942b19f18
Glabay <Java_Guru@live.com>
2026-05-14 02:01:11 -0300

    chore: loading maps from the cache instead of resource files
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/dev/midnightcoder/rpg/assets/PngMapLoader.java
+++ src/main/java/dev/midnightcoder/rpg/assets/PngMapLoader.java
@@ -1,11 +1,14 @@
 package dev.midnightcoder.rpg.assets;
 
+import dev.midnightcoder.cache.CacheReader;
 import dev.midnightcoder.engine.world.TileMap;
 import dev.midnightcoder.engine.world.loader.MapLoader;
 import dev.midnightcoder.engine.world.tile.Tile;
 import dev.midnightcoder.rpg.assets.tiles.TileColorRegistry;
 
 import javax.imageio.ImageIO;
+import java.io.ByteArrayInputStream;
+import java.util.UUID;
 
 /**
  * @author Glabay | Glabay-Studios
@@ -15,22 +18,21 @@ import javax.imageio.ImageIO;
  */
 public class PngMapLoader extends MapLoader {
 
-    public TileMap loadMapFile(String path) {
+    public TileMap loadMapFile(String uuid) {
         try {
-            var file = getClass().getResourceAsStream(path);
-            if (file == null)
-                throw new RuntimeException("Map file not found: " + path);
-
-            var image = ImageIO.read(file);
+            var cacheReader = CacheReader.getInstance();
+            var mapDefinition = cacheReader.getCacheManager().getMap(UUID.fromString(uuid));
+            var mapBytes = mapDefinition.getPngData();
+            var image = ImageIO.read(new ByteArrayInputStream(mapBytes));
+            if (image == null)
+                throw new RuntimeException("Failed to load map image from bytes: " + uuid);
 
             var width = image.getWidth();
             var height = image.getHeight();
-
             var tileMap = new TileMap(width, height);
 
             for (int x = 0; x < width; x++) {
                 for (int y = 0; y < height; y++) {
-
                     var pixel = image.getRGB(x, y);
                     var hexStr = String.format("0x%08X", pixel);
                     var tileType = TileColorRegistry.getInstance().getTileType(hexStr);
@@ -42,7 +44,7 @@ public class PngMapLoader extends MapLoader {
             return tileMap;
         }
         catch (Exception e) {
-            throw new RuntimeException("Failed to load map file: " + path, e);
+            throw new RuntimeException("Failed to load map file: " + uuid, e);
         }
     }
 }
