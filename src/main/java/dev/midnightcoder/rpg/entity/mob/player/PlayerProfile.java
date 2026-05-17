741c2f480c8dddd7e01c355a0e4c3c0edd640371
Glabay <Java_Guru@live.com>
2026-05-16 23:13:26 -0300

    chore: aded master volume to profile
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/dev/midnightcoder/rpg/entity/mob/player/PlayerProfile.java
+++ src/main/java/dev/midnightcoder/rpg/entity/mob/player/PlayerProfile.java
@@ -7,7 +7,13 @@ package dev.midnightcoder.rpg.entity.mob.player;
  * @since 2026-05-03
  */
 public class PlayerProfile {
+    /// TODO: Export this profile and data to a file for saving and loading
+    ///       This will be temp for "Offline-Mode" until we introduce a network
+    ///       At that point a DTO will be made, and passed over the network for saving
+
     private final String username;
+
+    private float masterVolume = 0.5f;
 
     public PlayerProfile(String username) {
         this.username = username;
@@ -15,5 +21,13 @@ public class PlayerProfile {
 
     public String getUsername() {
         return username;
+    }
+
+    public float getMasterVolume() {
+        return masterVolume;
+    }
+
+    public void setMasterVolume(float masterVolume) {
+        this.masterVolume = masterVolume;
     }
 }
