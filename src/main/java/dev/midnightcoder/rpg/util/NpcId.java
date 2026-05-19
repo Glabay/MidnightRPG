c4172ce9824da038babfd98d9cb1585cc9ec24ad
Glabay <Java_Guru@live.com>
2026-05-19 13:09:35 -0300

    chore: added NPC Identities
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/dev/midnightcoder/rpg/util/NpcId.java
+++ src/main/java/dev/midnightcoder/rpg/util/NpcId.java
@@ -9,4 +9,7 @@ package dev.midnightcoder.rpg.util;
 public class NpcId {
 
     public static final int POKEY = 0;
+    public static final int CHICKEN = 1;
+    public static final int STONE_GOLEM = 2;
+    public static final int OCEANFAERY = 3;
 }
