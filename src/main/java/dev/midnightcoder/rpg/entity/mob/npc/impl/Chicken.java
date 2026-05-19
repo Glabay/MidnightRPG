66bc42472155ba576526dbd45318ebd6fe808d0c
Glabay <Java_Guru@live.com>
2026-05-19 13:07:00 -0300

    chore: Added new NPC
    Signed-off-by: Glabay <Java_Guru@live.com>

---
+++ src/main/java/dev/midnightcoder/rpg/entity/mob/npc/impl/Chicken.java
@@ -0,0 +1,52 @@
+package dev.midnightcoder.rpg.entity.mob.npc.impl;
+
+import dev.midnightcoder.engine.util.Vec2i;
+import dev.midnightcoder.engine.world.GameMap;
+import dev.midnightcoder.rpg.MidnightRPG;
+import dev.midnightcoder.rpg.entity.mob.npc.NPC;
+import dev.midnightcoder.rpg.entity.mob.npc.behaviors.CombatBehavior;
+import dev.midnightcoder.rpg.entity.mob.npc.behaviors.WanderBehavior;
+import dev.midnightcoder.rpg.util.NpcId;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project MidnightRPG
+ * @social Discord: Glabay
+ * @since 2026-05-19
+ */
+public class Chicken extends NPC {
+
+    public Chicken(int tileX, int tileY, GameMap currentMap) {
+        super(NpcId.CHICKEN, new Vec2i(tileX << 5, tileY << 5), currentMap);
+        addBehavior(new WanderBehavior());
+        addBehavior(new CombatBehavior());
+    }
+
+    @Override
+    protected int getInteractionDistance() {
+        return 2;
+    }
+
+    @Override
+    public void handleMenuOption(String option) {
+        var player = MidnightRPG.getInstance().getGameScreen().getPlayer();
+        switch (option.toLowerCase()) {
+            case "attack" -> {
+                // if the user is too far, send a dialogue message
+                if (!entityWithinDist(this, getInteractionDistance())) {
+                    MidnightRPG.getInstance()
+                        .getGameScreen()
+                        .getDialogueInterface()
+                        .sendInfoInter("Too far away", "You are too far away to interact with this.");
+                    return;
+                }
+                // TODO: Implement Combat System
+            }
+            case "examine" ->
+                MidnightRPG.getInstance()
+                    .getGameScreen()
+                    .getDialogueInterface()
+                    .sendInfoInter(getDefinition().getName(), getDefinition().getDescription());
+        }
+    }
+}
