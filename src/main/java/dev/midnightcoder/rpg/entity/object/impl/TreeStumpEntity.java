1ba77d27375e7de02b9d2f123bfdedd17f3963a5
Glabay <Java_Guru@live.com>
2026-06-08 08:35:12 -0300

    chore: moved file to imple folder
    Signed-off-by: Glabay <Java_Guru@live.com>

---
+++ src/main/java/dev/midnightcoder/rpg/entity/object/impl/TreeStumpEntity.java
@@ -0,0 +1,17 @@
+package dev.midnightcoder.rpg.entity.object.impl;
+
+import dev.midnightcoder.engine.util.Vec2i;
+import dev.midnightcoder.engine.world.GameMap;
+import dev.midnightcoder.rpg.entity.object.GameObject;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project MidnightRPG
+ * @social Discord: Glabay
+ * @since 2026-05-21
+ */
+public class TreeStumpEntity extends GameObject {
+    public TreeStumpEntity(GameMap currentMap, Vec2i position) {
+        super(currentMap, position);
+    }
+}
