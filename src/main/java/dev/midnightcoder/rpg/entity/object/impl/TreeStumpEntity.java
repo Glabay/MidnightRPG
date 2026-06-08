e2089f27fd2d1079f85516d30c5f1fbc63574327
Glabay <Java_Guru@live.com>
2026-06-08 09:18:41 -0300

    chore: added ObjectId class to manage object IDs
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/dev/midnightcoder/rpg/entity/object/impl/TreeStumpEntity.java
+++ src/main/java/dev/midnightcoder/rpg/entity/object/impl/TreeStumpEntity.java
@@ -3,6 +3,7 @@ package dev.midnightcoder.rpg.entity.object.impl;
 import dev.midnightcoder.engine.util.Vec2i;
 import dev.midnightcoder.engine.world.GameMap;
 import dev.midnightcoder.rpg.entity.object.GameObject;
+import dev.midnightcoder.rpg.util.ObjectId;
 
 /**
  * @author Glabay | Glabay-Studios
@@ -13,5 +14,10 @@ import dev.midnightcoder.rpg.entity.object.GameObject;
 public class TreeStumpEntity extends GameObject {
     public TreeStumpEntity(GameMap currentMap, Vec2i position) {
         super(currentMap, position);
+    }
+
+    @Override
+    protected int getObjectId() {
+        return ObjectId.NORMAL_TREE_STUMP;
     }
 }
---
+++ src/main/java/dev/midnightcoder/rpg/util/ObjectId.java
@@ -0,0 +1,12 @@
+package dev.midnightcoder.rpg.util;
+
+/**
+ * @author Glabay | Glabay-Studios
+ * @project MidnightRPG
+ * @social Discord: Glabay
+ * @since 2026-06-08
+ */
+public class ObjectId {
+    public static final int STONE_ROCK = 0;
+    public static final int NORMAL_TREE_STUMP = 1;
+}
